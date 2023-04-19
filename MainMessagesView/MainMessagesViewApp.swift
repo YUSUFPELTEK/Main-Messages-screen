26e0cf3849d56308ea4290489b7aee50952edca5
YUSUFPELTEK <yusufpeltek0@gmail.com>
2023-04-20 02:41:23 +0300

    Initial Commit

---
+++ MainMessagesView/MainMessagesViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  MainMessagesViewApp.swift
+//  MainMessagesView
+//
+//  Created by YUSUF on 20.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct MainMessagesViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
